9b481513b08da95527f7b9e336f371ac1618aba4
Justin Meyer <justinbmeyer@gmail.com>
2019-01-31 11:50:29 -0600

    interfaces part 1

--- 4-classes.spec.ts
+++ 4-classes.spec.ts
@@ -26,8 +26,9 @@ describe('Classes Specialist', () => {
 	it('basics work', () => {
 		let employee2 = new Specialist("Owen", 14);
 		assert.equal( employee2.sayHi(), `Owen says "hi"` );
-		assert.equal(removeSpaces(employee2.safetyQuote()), removeSpaces(`Never turn your back to the cage.
-		Trust me, I have 14 years of experience`));
+		assert.equal(removeSpaces(employee2.safetyQuote()),
+			removeSpaces(`Never turn your back to the cage.
+				Trust me, I have 14 years of experience`));
 	});
 
 });
