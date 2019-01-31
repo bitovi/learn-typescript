0fcc40a59ecb5c676b9e7641648a178264a6e634
Justin Meyer <justinbmeyer@gmail.com>
2019-01-31 10:20:33 -0600

    adding specialist

--- 4-classes.spec.ts
+++ 4-classes.spec.ts
@@ -1,4 +1,5 @@
 import DinoKeeper from './4a-classes-hello-dino';
+import Specialist from './4a-specialist';
 import { strict as assert } from 'assert';
 import 'mocha';
 
@@ -12,6 +13,21 @@ describe('Classes DinoKeeper', () => {
 	it('typing works', () => {
 		var dinoKeeper = new DinoKeeper("Joe") as DinoKeeper;
 		assert.equal(dinoKeeper.sayHi() , `Joe says "hi"`);
+	});
+
+});
+
+function removeSpaces(str: string) {
+	return str.replace(/\s+/g," ");
+}
+
+describe('Classes Specialist', () => {
+
+	it('basics work', () => {
+		let employee2 = new Specialist("Owen", 14);
+		assert.equal( employee2.sayHi(), `Owen says "hi"` );
+		assert.equal(removeSpaces(employee2.safetyQuote()), removeSpaces(`Never turn your back to the cage.
+		Trust me, I have 14 years of experience`));
 	});
 
 });
